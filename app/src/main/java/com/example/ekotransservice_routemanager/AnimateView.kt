6366054d7eb6752846a472d3e23f3bffaf7390ae
alexey <vaneev@eko-ekb.ru>
2020-09-16 10:30:03 +0500

    create

---
+++ app/src/main/java/com/example/ekotransservice_routemanager/AnimateView.kt
@@ -0,0 +1,77 @@
+package com.example.ekotransservice_routemanager
+
+import android.content.Context
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.children
+
+class AnimateView (var view : View, var context: Context){
+
+    fun hideHeight (){
+        val set = ConstraintSet()
+        set.clone(view as ConstraintLayout)
+        set.connect(view.id,ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP)
+        for (child in (view as ConstraintLayout).children){
+            set.constrainHeight(child.id,0)
+            set.setVisibility(child.id,View.GONE)
+        }
+
+        //
+        val autoTransition = AutoTransition()
+        autoTransition.duration = 300;
+        TransitionManager.beginDelayedTransition(view as ConstraintLayout,autoTransition)
+        set.applyTo(view as ConstraintLayout)
+        /*val hide = AnimationUtils.loadAnimation(context,R.anim.hide_height)
+        view.startAnimation(hide)
+        hide.setAnimationListener(object  : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                view.visibility = View.GONE
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })*/
+    }
+
+    fun showHeight(){
+        /*val show = AnimationUtils.loadAnimation(context,R.anim.hide_height)
+        view.startAnimation(show)
+        view.visibility = ViewGroup.VISIBLE*/
+
+        val set = ConstraintSet()
+        set.clone(view as ConstraintLayout)
+
+        view.visibility = ViewGroup.VISIBLE
+        for (child in (view as ConstraintLayout).children){
+            set.constrainHeight(child.id,ConstraintSet.WRAP_CONTENT)
+            set.setVisibility(child.id,View.VISIBLE)
+        }
+        val autoTransition = AutoTransition()
+        autoTransition.duration = 300;
+        TransitionManager.beginDelayedTransition(view as ConstraintLayout,autoTransition)
+        set.applyTo(view as ConstraintLayout)
+
+    }
+
+    fun rotate (){
+        val rotate = AnimationUtils.loadAnimation(this.context,R.anim.routate_pict)
+        view.startAnimation(rotate)
+    }
+
+    fun rotateBack (){
+        val rotate = AnimationUtils.loadAnimation(this.context,R.anim.rotate_pict_back)
+        view.startAnimation(rotate)
+    }
+}
--- app/src/main/java/com/example/ekotransservice_routemanager/start_frame_screen.kt
+++ app/src/main/java/com/example/ekotransservice_routemanager/start_frame_screen.kt
@@ -1,14 +1,23 @@
 package com.example.ekotransservice_routemanager
 
+import android.animation.Animator
+import android.animation.StateListAnimator
+import android.animation.ValueAnimator
+import android.graphics.drawable.Drawable
+import android.opengl.Visibility
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
+import androidx.core.animation.addListener
 
 class start_frame_screen : Fragment() {
-
+    var closedRoute : Boolean = false
     companion object {
         fun newInstance() = start_frame_screen()
     }
@@ -19,7 +28,15 @@ class start_frame_screen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.start_frame_screen_fragment, container, false)
+        val mainView = inflater.inflate(R.layout.start_frame_screen_fragment, container, false)
+        val closeView : View = mainView.findViewById<View>(R.id.layoutToCloseRoute)
+
+        closeView.setOnClickListener {
+            showHideCloseRoute(mainView)
+        }
+
+        showHideCloseRoute(mainView)
+        return mainView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,4 +45,31 @@ class start_frame_screen : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    fun showHideCloseRoute(mainView : View){
+        val toCloseView = mainView.findViewById<View>(R.id.closeLayout)
+        if(!closedRoute){
+            val animateView = this.context?.let { it1 -> AnimateView(toCloseView, it1) }
+            animateView!!.hideHeight()
+        }else{
+            val animateView = this.context?.let { it1 -> AnimateView(toCloseView, it1) }
+            animateView!!.showHeight()
+        }
+
+
+        val imageRoutate = mainView.findViewById<View>(R.id.imageOpenCloseRoute)
+
+        if(!closedRoute){
+            val animateView = this.context?.let { it1 -> AnimateView(imageRoutate, it1) }
+            animateView!!.rotate()
+        }else{
+            val animateView = this.context?.let { it1 -> AnimateView(imageRoutate, it1) }
+            animateView!!.rotateBack()
+        }
+
+        closedRoute = !closedRoute
+    }
+
 }
+
+
+
