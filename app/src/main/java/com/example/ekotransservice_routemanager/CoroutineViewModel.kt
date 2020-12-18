0d293845bc95a5e31339a28e161704122ad4803f
alexey <vaneev@eko-ekb.ru>
2020-12-18 09:58:19 +0500

    try catch in coroutine view model

--- app/src/main/java/com/example/ekotransservice_routemanager/CoroutineViewModel.kt
+++ app/src/main/java/com/example/ekotransservice_routemanager/CoroutineViewModel.kt
@@ -2,17 +2,24 @@ package com.example.ekotransservice_routemanager
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class CoroutineViewModel(val activity: MainActivity,val coroutineWork : suspend () -> Unit, val afterWork : () -> Unit) : ViewModel() {
+class CoroutineViewModel(val activity: MainActivity,
+                         val coroutineWork : suspend () -> Unit,
+                         val afterWork : () -> Unit) : ViewModel() {
     private val workDoneCheck : MutableLiveData<Boolean> = MutableLiveData(false)
 
     init{
         workDoneCheck.removeObservers(activity)
-        workDoneCheck.observe(activity, Observer {if(it){ afterWork() }})
+        workDoneCheck.observe(activity, {
+            if(it) {
+                afterWork()
+                workDoneCheck.value = false
+            }
+
+        })
     }
 
     fun startWork (){
